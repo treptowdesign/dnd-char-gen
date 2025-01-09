16eecf1aae67d3d5dad0743e8f13c4ae0c432df7
Kyle Treptow <kyletreptowdesign@gmail.com>
2025-01-08 20:44:27 -0800

    dashboard sass modules

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,17 @@
-import styles from "@/app/page.module.css";
+// import styles from "@/app/page.module.css";
 import PageNav from '@/app/components/PageNav';
+
+import styles from "@/app/dashboard/dashboard.module.sass";
 
 export default function Dashboard() {
     return (
       <div className={styles.page}>
         <main className={styles.main}>
+          <div className={styles.container}>
             <PageNav />
-            <h1>Dashboard</h1>
-            <p>This is the dashboard page.</p>
+            <h1 className={styles.title}>Dashboard</h1>
+            <button className={styles.button}>Click Me</button>
+          </div>
         </main>
       </div>
     );
