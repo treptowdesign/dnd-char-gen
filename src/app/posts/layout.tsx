4b5c9e69db4b28f57b12459e05baab4c31daa357
Kyle Treptow <kyletreptowdesign@gmail.com>
2025-01-15 18:25:21 -0800

    feature - posts layout added

---
+++ src/app/posts/layout.tsx
@@ -0,0 +1,9 @@
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+    return (
+      <div>
+        <h1>Posts Layout</h1>
+        {children}
+      </div>
+    );
+  }
+  
