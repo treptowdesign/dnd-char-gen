c17a83bd3d4cd12e77bd94734641ffe54bf58898
Kyle Treptow <kyletreptowdesign@gmail.com>
2025-01-16 06:33:21 -0800

    auth - use lib/auth for protected route

--- src/app/api/protected/route.ts
+++ src/app/api/protected/route.ts
@@ -1,33 +1,17 @@
 import { NextResponse } from 'next/server';
-import { cookies } from 'next/headers';
-import jwt from 'jsonwebtoken';
-
-const SECRET_KEY = process.env.JWT_SECRET as string;
-if (!SECRET_KEY) {
-  throw new Error('Missing JWT_SECRET in environment variables');
-}
+import { getUserFromServer } from "@/app/lib/auth"; // server side auth
 
 export async function GET() {
-  // get cookies from the request
-  const cookieStore = await cookies();
-  const token = cookieStore.get('chargen_authToken_server')?.value;
-
-  // if no token, return unauthorized
-  if (!token) {
-    return NextResponse.json({ error: 'Unauthorized: No token' }, { status: 401 });
-  }
-
   try {
-    // verify the token
-    const decoded = jwt.verify(token, SECRET_KEY) as { id: number; email: string };
-
+    const user = await getUserFromServer();
+    if (!user) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     // return a protected response
     return NextResponse.json({
       message: 'Protected route accessed successfully!',
-      user: { id: decoded.id, email: decoded.email },
+      user: { id: user.id, email: user.email },
     });
   } catch (error) {
-    console.error('Invalid or expired token:', error);
-    return NextResponse.json({ error: 'Unauthorized: Invalid token' }, { status: 401 });
+    console.error('Auth Error:', error);
+    return NextResponse.json({ error: 'Auth Error' }, { status: 401 });
   }
 }
