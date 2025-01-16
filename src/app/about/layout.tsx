83e888254344c91ebc5ba3005f0d382645a478dc
Kyle Treptow <kyletreptowdesign@gmail.com>
2025-01-16 07:00:38 -0800

    about - layout, page, and nav

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -1,7 +1,7 @@
 export default function AboutLayout({ children }: { children: React.ReactNode }) {
     return (
       <div>
-        <h1>About Layout</h1>
+        {/* <h1>About Layout</h1> */}
         {children}
       </div>
     );
